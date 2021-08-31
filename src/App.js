1be04bed756238dbd7ad5abfd4d184d3c9458e49
Yayak Yogi <yayaktaka@gmail.com>
2021-08-31 14:56:11 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./assets/scss/style.scss";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  return <div className="App">hai app</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
