ee479adc2427b29c21783527d59863dadf4cabde
Yayak Yogi <yayaktaka@gmail.com>
2021-08-31 14:58:11 +0700

    fix header

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,12 @@
-import React from "react";
-import Button from "";
+import React, { Component } from "react";
+import Header from "../parts/Header";
 
-export default function LandingPage() {
-  return <div></div>;
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <>
+        <Header {...this.props}></Header>
+      </>
+    );
+  }
 }
