0e0a93f23f3961f9bdb5080a91721b8987db7504
Yayak Yogi <yayaktaka@gmail.com>
2021-09-30 07:45:51 +0700

    first commit

---
+++ src/parts/Testimoni.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Star from "../elements/Star";
+
+export default function Testimoni() {
+  return (
+    <div>
+      <Star value={3.3} width={35} height={35} spacing={4}></Star>
+    </div>
+  );
+}
