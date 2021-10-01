f057d4e75f80827dadca6a1a1d780d200cd25615
Yayak Yogi <yayaktaka@gmail.com>
2021-10-01 15:05:10 +0700

    fix component inputDate

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 
-import InputNumber from "../elements/Form/InputNumber";
+import { InputDate } from "../elements/Form";
 
 export default class Example extends Component {
   state = {
-    value: "1",
+    value: {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
   };
 
+  // handleChange = (e) => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({ value: e.target.value });
   };
 
   render() {
@@ -19,10 +26,16 @@ export default class Example extends Component {
           style={{ height: "100vh" }}
         >
           <div className="col-auto">
-            <InputNumber
+            {/* <InputNumber
               max={30}
               suffix=" night"
               isSuffixPlural
+              onChange={this.handleChange}
+              name="value"
+              value={this.state.value}
+            /> */}
+            <InputDate
+              max={30}
               onChange={this.handleChange}
               name="value"
               value={this.state.value}
