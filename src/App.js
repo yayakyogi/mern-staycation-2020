1c5caee0acd1ddfd707e19a7909d91917e2c8f1b
Yayak Yogi <yayaktaka@gmail.com>
2021-10-01 10:43:17 +0700

    add route for page example

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "./assets/scss/style.scss";
 import LandingPage from "./pages/LandingPage";
+import Example from "./pages/Example";
 
 function App() {
   useEffect(() => {
@@ -13,7 +14,8 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage}></Route>
+        <Route path="/example" component={Example}></Route>
       </Router>
     </div>
   );
