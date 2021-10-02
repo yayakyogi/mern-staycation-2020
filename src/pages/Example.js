69ee433eb642c333d6d2fcba152a3e3c2c519f4d
Yayak Yogi <yayaktaka@gmail.com>
2021-10-03 05:26:47 +0700

    try component inputDate"

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,24 +1,13 @@
 import React, { Component } from "react";
 
-import { InputDate } from "../elements/Form";
+import Breadcrumb from "../elements/Breadcrumb";
 
 export default class Example extends Component {
-  state = {
-    value: {
-      startDate: new Date(),
-      endDate: new Date(),
-      key: "selection",
-    },
-  };
-
-  // handleChange = (e) => {
-  //   this.setState({ [e.target.name]: e.target.value });
-  // };
-  handleChange = (e) => {
-    this.setState({ value: e.target.value });
-  };
-
   render() {
+    const breadcrumbList = [
+      { pageTitle: "Home", pageHref: "" },
+      { pageTitle: "House Details", pageHref: "" },
+    ];
     return (
       <div className="container">
         <div
@@ -26,20 +15,7 @@ export default class Example extends Component {
           style={{ height: "100vh" }}
         >
           <div className="col-auto">
-            {/* <InputNumber
-              max={30}
-              suffix=" night"
-              isSuffixPlural
-              onChange={this.handleChange}
-              name="value"
-              value={this.state.value}
-            /> */}
-            <InputDate
-              max={30}
-              onChange={this.handleChange}
-              name="value"
-              value={this.state.value}
-            />
+            <Breadcrumb data={breadcrumbList} />
           </div>
         </div>
       </div>
