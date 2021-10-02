80074344b837f1c9be9dccdb7252d1554a634355
Yayak Yogi <yayaktaka@gmail.com>
2021-10-03 05:28:25 +0700

    change route icon parts hero

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -14,6 +14,10 @@ export default class LandingPage extends Component {
     super(props);
     this.refMostPicked = React.createRef(); // buat ref baru dari React.createRef()
   }
+  componentDidMount() {
+    window.title = "Staycation | Home";
+    window.scrollTo(0, 0);
+  }
   render() {
     return (
       <>
