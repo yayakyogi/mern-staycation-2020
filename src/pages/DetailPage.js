f935d3d45b950a3219dbf0f0f5d20cc75fe2350a
Yayak Yogi <yayaktaka@gmail.com>
2021-10-03 08:26:49 +0700

    add aos and elements categories,tertimony,footer

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -4,6 +4,10 @@ import Header from "../parts/Header";
 import PageDetailTitle from "../parts/PageDetailTitle";
 import FeatureImage from "../parts/FeatureImage";
 import PageDetailDescription from "../parts/PageDetailDescription";
+import BookingForm from "../parts/BookingForm";
+import Categories from "../parts/Categories";
+import Testimoni from "../parts/Testimoni";
+import Footer from "../parts/Footer";
 
 import itemDetails from "../json/itemDetails.json";
 
@@ -25,12 +29,24 @@ export default class DetailsPage extends Component {
           data={itemDetails}
         ></PageDetailTitle>
         <FeatureImage data={itemDetails.imageUrls}></FeatureImage>
-        <section className="container">
-          <div className="col-7 pr-5">
-            <PageDetailDescription data={itemDetails}></PageDetailDescription>
+        <section
+          className="container"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          <div className="row">
+            <div className="col-7 pr-5">
+              <PageDetailDescription data={itemDetails}></PageDetailDescription>
+            </div>
+            <div className="col-5">
+              <BookingForm itemDetails={itemDetails}></BookingForm>
+              {console.log("itemDetail from pagedetails: ", itemDetails)}
+            </div>
           </div>
-          <div className="col-5"></div>
         </section>
+        <Categories data={itemDetails.categories} />
+        <Testimoni data={itemDetails.testimonial} />
+        <Footer />
       </>
     );
   }
